9af7fb4660295facf57c017295c966de1b2b2526
raisammer <93609977+raisammer@users.noreply.github.com>
2024-04-26 22:37:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import cookieParser from 'cookie-parser'
 dotenv.config()
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({credentials:true,origin:'*'}))
 app.use(cookieParser());
 
 connectDB()
